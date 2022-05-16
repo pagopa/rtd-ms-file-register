39660788931cd7edaef40f11e918f30e7f494a8a
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-16 11:40:01 +0200

    [RTD-9] add application main

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/RtdMsFileRegisterApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.rtd.ms.rtdmsfileregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtdMsFileRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtdMsFileRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/RtdMsFileRegisterApplicationTests.java
@@ -0,0 +1,16 @@
+package it.gov.pagopa.rtd.ms.rtdmsfileregister;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtdMsFileRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
