1bbfcc5dcdd1527fe6666f9e0591768d1ce0d73b
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-18 18:10:46 +0200

    [RTD-94] add RTD file entity

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/RtdFile.java
@@ -0,0 +1,51 @@
+package it.gov.pagopa.rtd.ms.rtdmsfileregister;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RtdFile implements Serializable {
+
+  @Id
+  private String name;
+
+  @Pattern(regexp = "[a-zA-Z0-9]{64}",
+      message = "Hash length must be 64 alphanumeric char")
+  private String hash;
+
+  @Min(value = 0, message = "The value must be positive")
+  private int numTrx;
+
+  @Min(value = 0, message = "The value must be positive")
+  private int numAggregates;
+
+  @Min(value = 0L, message = "The value must be positive")
+  private BigDecimal amountAde;
+
+  @Min(value = 0L, message = "The value must be positive")
+  private BigDecimal amountRtd;
+
+  @Min(value = 0, message = "The value must be positive")
+  private int numChunks;
+
+  @NotNull
+  @NotBlank
+  private int status;
+
+}
