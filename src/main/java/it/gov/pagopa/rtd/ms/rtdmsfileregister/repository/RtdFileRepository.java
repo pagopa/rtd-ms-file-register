2c01f2607c5966fdc2f065b461b375cec5521df6
Giovanni <g.mancini.0@gmail.com>
2022-05-20 07:21:51 +0200

    [RTD-94] Add sample connection strings

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/repository/RtdFileRepository.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/repository/RtdFileRepository.java
@@ -1,17 +1,29 @@
 package it.gov.pagopa.rtd.ms.rtdmsfileregister.repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.model.RtdFile;
 
+// import org.springframework.data.repository.CrudRepository;
+// import org.springframework.stereotype.Repository;
+
 /**
- * Data Access Object to manage all CRUD operations to the database
+ * Data Access Object to manage all CRUD operations to the database This kind of repository
+ * leverages object relational mappong (ORM), so it can be used only with relational DB.
+ * 
  */
-@Repository
-public interface RtdFileRepository extends CrudRepository<RtdFile, String> {
+// @Repository
+// public interface RtdFileRepository extends CrudRepository<RtdFile, String> {
 
-  RtdFile findByName(String name);
+//   RtdFile findByName(String name);
 
-  List<RtdFile> findByStatus(int status);
+//   List<RtdFile> findByStatus(int status);
+// }
+
+
+
+public interface RtdFileRepository extends MongoRepository<RtdFile, String> {
+
+  List<RtdFile> findByEnabled(boolean enabled);
+
 }
