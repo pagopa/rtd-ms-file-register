3d6b17ba6479e33d6d44eec69bbbdc878bf3cae6
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-24 10:44:01 +0200

    [RTD-94] add rtd CRUD service

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/service/RtdFileCrudService.java
@@ -0,0 +1,18 @@
+package it.gov.pagopa.rtd.ms.rtdmsfileregister.service;
+
+import it.gov.pagopa.rtd.ms.rtdmsfileregister.model.RtdFile;
+import it.gov.pagopa.rtd.ms.rtdmsfileregister.repository.RtdFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RtdFileCrudService {
+
+  @Autowired
+  private RtdFileRepository repository;
+
+  public boolean storeRtdFileStatus(RtdFile f){
+    repository.save(f);
+    return true;
+  }
+}
