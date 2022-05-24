b13ad76ddab37c04e097c0b09717d05fb1aad601
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-24 12:10:49 +0200

    [RTD-94] remove annotations

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/model/RtdFile.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/model/RtdFile.java
@@ -2,9 +2,6 @@ package it.gov.pagopa.rtd.ms.rtdmsfileregister.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -13,39 +10,34 @@ import lombok.Setter;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
 @Data
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class RtdFile implements Serializable {
+@Document("fileregister")
+public class RtdFile {
 
-  @Id
   private String name;
 
-  @Pattern(regexp = "[a-zA-Z0-9]{64}",
-      message = "Hash length must be 64 alphanumeric char")
   private String hash;
 
-  @Min(value = 0, message = "The value must be positive")
+
   private int numTrx;
 
-  @Min(value = 0, message = "The value must be positive")
+
   private int numAggregates;
 
-  @Min(value = 0L, message = "The value must be positive")
+
   private BigDecimal amountAde;
 
-  @Min(value = 0L, message = "The value must be positive")
+
   private BigDecimal amountRtd;
 
-  @Min(value = 0, message = "The value must be positive")
   private int numChunks;
 
-  @NotNull
-  @NotBlank
   private int status;
 
 }
