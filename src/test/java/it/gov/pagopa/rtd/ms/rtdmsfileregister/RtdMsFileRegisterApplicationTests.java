5553949bd9a8a1beb4306e426c88caaa3b60fc5b
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-24 11:03:12 +0200

    [RTD-94] restore app test

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/RtdMsFileRegisterApplicationTests.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/RtdMsFileRegisterApplicationTests.java
@@ -1,16 +1,31 @@
-//package it.gov.pagopa.rtd.ms.rtdmsfileregister;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class RtdMsFileRegisterApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//		assertTrue(true);
-//	}
-//
-//}
+package it.gov.pagopa.rtd.ms.rtdmsfileregister;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import it.gov.pagopa.rtd.ms.rtdmsfileregister.controller.RestControllerImpl;
+import it.gov.pagopa.rtd.ms.rtdmsfileregister.repository.RtdFileRepository;
+import it.gov.pagopa.rtd.ms.rtdmsfileregister.service.RtdFileCrudService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+class RtdMsFileRegisterApplicationTests {
+
+  @MockBean
+  RtdFileRepository repository;
+
+	@InjectMocks
+	RtdFileCrudService service;
+
+	@Autowired
+	RestControllerImpl controller;
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
