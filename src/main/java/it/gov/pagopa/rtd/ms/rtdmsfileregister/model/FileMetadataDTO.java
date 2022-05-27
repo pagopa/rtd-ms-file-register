6e9c7af1f81c7d17578493fef26fdd28e9114143
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-27 15:44:56 +0200

    [RTD-127] add DTO

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/model/FileMetadataDTO.java
@@ -0,0 +1,32 @@
+package it.gov.pagopa.rtd.ms.rtdmsfileregister.model;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileMetadataDTO {
+
+  private String name;
+
+  private String hash;
+
+  private int numTrx;
+
+  private int numAggregates;
+
+  private BigDecimal amountAde;
+
+  private BigDecimal amountRtd;
+
+  private int numChunks;
+
+  private int status;
+}
