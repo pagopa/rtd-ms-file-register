a3b5c5453d404e632d7a9fc429739022f754e643
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-31 16:43:51 +0200

    [RTD-126] refactor ReST controller

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/controller/RestController.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/controller/RestController.java
@@ -3,29 +3,37 @@ package it.gov.pagopa.rtd.ms.rtdmsfileregister.controller;
 import io.swagger.annotations.Api;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.model.FileMetadataDTO;
 import java.util.List;
-import org.springframework.http.ResponseEntity;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * Controller to expose MicroService
+ * Controller to expose RTD File Register MicroService's endpoints
  */
 @Api(tags = "RTD file register Controller")
 @RequestMapping("")
 @Validated
 public interface RestController {
 
-  @GetMapping(value = "/file-status")
-  List<FileMetadataDTO> getFileStatus();
+  @GetMapping(value = "/file-status", produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.OK)
+  List<FileMetadataDTO> getFileMetadata(@RequestParam @Valid @NotNull @NotBlank String filename);
 
+  @PutMapping(value = "/file-status", produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.OK)
+  FileMetadataDTO putFileMetadata(@NotNull @NotBlank @RequestBody FileMetadataDTO body);
 
-  @PutMapping(value = "/file-status")
-  ResponseEntity<String> setFileStatus(@RequestBody FileMetadataDTO body);
-
-  @DeleteMapping(value = "/file-status")
-  ResponseEntity<String> deleteFileMetadata();
+  @DeleteMapping(value = "/file-status", produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.OK)
+  List<FileMetadataDTO> deleteFileMetadata(@NotNull @NotBlank @RequestParam String filename);
 }
