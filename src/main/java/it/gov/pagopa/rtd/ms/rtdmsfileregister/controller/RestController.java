62e3dd6c05a2c3ceeba38eb194e7b09cee9ad6b7
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-31 15:40:55 +0200

    [RTD-126] remove explicit health check endpoint

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/controller/RestController.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/controller/RestController.java
@@ -3,7 +3,6 @@ package it.gov.pagopa.rtd.ms.rtdmsfileregister.controller;
 import io.swagger.annotations.Api;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.model.FileMetadataDTO;
 import java.util.List;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Controller to expose MicroService
@@ -20,10 +18,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping("")
 @Validated
 public interface RestController {
-
-  @GetMapping(value = "/")
-  @ResponseStatus(HttpStatus.OK)
-  void healthCheck();
 
   @GetMapping(value = "/file-status")
   List<FileMetadataDTO> getFileStatus();
