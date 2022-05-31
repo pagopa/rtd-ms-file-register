dfbafe9a9b21ad8b9a6821ba72e1ca7314f6505f
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-05-31 16:44:23 +0200

    [RTD-126] refactor ReST controller implementation

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/controller/RestControllerImpl.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/controller/RestControllerImpl.java
@@ -3,11 +3,7 @@ package it.gov.pagopa.rtd.ms.rtdmsfileregister.controller;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.model.FileMetadataDTO;
 import java.math.BigDecimal;
 import java.util.List;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,20 +14,30 @@ public class RestControllerImpl implements
     it.gov.pagopa.rtd.ms.rtdmsfileregister.controller.RestController {
 
   @Override
-  public List<FileMetadataDTO> getFileStatus() {
-    FileMetadataDTO stub = new FileMetadataDTO("test0", "abc", 5, 2, new BigDecimal(900),
+  public List<FileMetadataDTO> getFileMetadata(String filename) {
+    log.info("Received GET [{}]", filename);
+
+    FileMetadataDTO stub = new FileMetadataDTO(filename, "abc", 5, 2, new BigDecimal(900),
         new BigDecimal(700), 5, 0);
+
     return List.of(stub);
   }
 
   @Override
-  public ResponseEntity<String> setFileStatus(@NotNull @NotBlank FileMetadataDTO body) {
-    return new ResponseEntity<>(HttpStatus.OK);
+  public FileMetadataDTO putFileMetadata(FileMetadataDTO body) {
+    log.info("Received PUT [{}]", body);
+
+    return body;
   }
 
   @Override
-  public ResponseEntity<String> deleteFileMetadata() {
-    return new ResponseEntity<>(HttpStatus.OK);
+  public List<FileMetadataDTO> deleteFileMetadata(String filename) {
+    log.info("Received DELETE [{}]", filename);
+
+    FileMetadataDTO stub = new FileMetadataDTO(filename, "abc", 5, 2, new BigDecimal(900),
+        new BigDecimal(700), 5, 0);
+
+    return List.of(stub);
   }
 
 }
