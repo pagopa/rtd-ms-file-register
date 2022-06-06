ee3941353814a48ddca9efd944d17b23d4628057
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-06-06 17:35:00 +0200

    [RTD-148] add file metadata entity

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/model/FileMetadataEntity.java
@@ -0,0 +1,54 @@
+package it.gov.pagopa.rtd.ms.rtdmsfileregister.model;
+
+import java.math.BigDecimal;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("fileregister")
+public class FileMetadataEntity {
+
+  @Id
+  @NotNull
+  @NotBlank
+  private String name;
+
+  @NotNull
+  @NotBlank
+  @Pattern(regexp = "[a-zA-Z0-9]{64}",
+      message = "Hash length must be 64 alphanumeric char")
+  private String hash;
+
+  @Min(value = 0, message = "The value must be positive")
+  private int numTrx;
+
+  @Min(value = 0, message = "The value must be positive")
+  private int numAggregates;
+
+  @Min(value = 0, message = "The value must be positive")
+  private BigDecimal amountAde;
+
+  @Min(value = 0, message = "The value must be positive")
+  private BigDecimal amountRtd;
+
+  @Min(value = 0, message = "The value must be positive")
+  private int numChunks;
+
+  @NotNull
+  @NotBlank
+  private int status;
+
+}
