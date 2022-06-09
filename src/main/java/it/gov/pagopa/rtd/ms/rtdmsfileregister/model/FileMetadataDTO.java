2a82f406c322cdbb769919acf4a4b8f07d724708
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-06-09 13:54:18 +0200

    [RTD-156] add specialized FileMetadataDTO class

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/model/FileMetadataDTO.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/model/FileMetadataDTO.java
@@ -1,48 +1,4 @@
 package it.gov.pagopa.rtd.ms.rtdmsfileregister.model;
 
-import java.math.BigDecimal;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Data
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class FileMetadataDTO {
-
-  @NotNull
-  @NotBlank
-  private String name;
-
-  @NotNull
-  @NotBlank
-  @Pattern(regexp = "[a-zA-Z0-9]{64}",
-      message = "Hash length must be 64 alphanumeric char")
-  private String hash;
-
-  @Min(value = 0, message = "numTrx value must be positive")
-  private Integer numTrx;
-
-  @Min(value = 0, message = "numAggregates value must be positive")
-  private Integer numAggregates;
-
-  @Min(value = 0, message = "amountAde value must be positive")
-  private BigDecimal amountAde;
-
-  @Min(value = 0, message = "amountRtd value must be positive")
-  private BigDecimal amountRtd;
-
-  @Min(value = 0, message = "numChunks value must be positive")
-  private Integer numChunks;
-
-  @NotNull
-  private Integer status;
+public class FileMetadataDTO extends FileMetadata{
 }
