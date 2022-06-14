3d706bf231ae0ecffb22e3a9e31510eb6b51e762
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-06-14 17:02:23 +0200

    [RTD-173] add event handler

---
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/event/EventHandler.java
@@ -0,0 +1,36 @@
+package it.gov.pagopa.rtd.ms.rtdmsfileregister.event;
+
+import it.gov.pagopa.rtd.ms.rtdmsfileregister.model.EventGridEvent;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+
+/**
+ * Component defining the processing steps in response to storage events.
+ */
+@Slf4j
+@Configuration
+@Getter
+public class EventHandler {
+
+  /**
+   * Constructor.
+   *
+   * @return a consumer for Event Grid events.
+   */
+  @Bean
+  public Consumer<Message<List<EventGridEvent>>> blobStorageConsumer() {
+    return message -> message.getPayload().stream()
+        .filter(e -> "Microsoft.Storage.BlobCreated".equals(e.getEventType()))
+        .map(m -> {
+          log.error("Received event: {}", m.getSubject());
+          return m;
+        })
+        .collect(Collectors.toList());
+  }
+}
