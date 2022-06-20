d902e4ce4a630fd1a904465c440edd2ecbcfa520
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-06-20 12:47:42 +0200

    [RTD-185] adapt service to only name and status annotated as not null

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/service/FileMetadataServiceTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/service/FileMetadataServiceTest.java
@@ -1,10 +1,9 @@
 package it.gov.pagopa.rtd.ms.rtdmsfileregister.service;
 
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.reset;
@@ -12,6 +11,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.RtdMsFileRegisterApplication;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.controller.RestController.DTOViolationException;
@@ -20,6 +20,7 @@ import it.gov.pagopa.rtd.ms.rtdmsfileregister.controller.RestController.Filename
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.model.FileMetadataDTO;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.model.FileMetadataEntity;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.repository.FileMetadataRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -46,9 +47,9 @@ class FileMetadataServiceTest {
   private FileMetadataEntity testFileMetadataEntity;
   private FileMetadataDTO newTestFileMetadataDTO;
 
-  static String testFileMetadataJSON = "{\"name\":\"presentFilename\",\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTrx\":5,\"numAggregates\":2,\"amountAde\":900,\"amountRtd\":700,\"numChunks\":5,\"status\":0}";
+  static String testFileMetadataJSON = "{\"name\":\"presentFilename\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTransactions\":5,\"numAggregates\":2,\"amountAggregates\":900,\"amountTransactions\":700,\"chunksTotal\":5,\"status\":0}";
 
-  static String newTestFileMetadataJSON = "{\"name\":\"newFilename\",\"hash\":\"090ed8c1103eb1dc4bae0ac2aa608fa5c085648438b7d38cfc238b9a98eba545\",\"numTrx\":15,\"numAggregates\":12,\"amountAde\":1900,\"amountRtd\":1700,\"numChunks\":15,\"status\":1}";
+  static String newTestFileMetadataJSON = "{\"name\":\"newFilename\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T12:19:16.500\",\"hash\":\"090ed8c1103eb1dc4bae0ac2aa608fa5c085648438b7d38cfc238b9a98eba545\",\"numTransactions\":15,\"numAggregates\":12,\"amountAggregates\":1900,\"amountTransactions\":1700,\"chunksTotal\":15,\"status\":1}";
 
   private String metadataUpdatesJSON = "{\"name\":\"presentFilename\", \"status\":1}";
 
@@ -60,12 +61,11 @@ class FileMetadataServiceTest {
 
     ObjectMapper objectMapper = new ObjectMapper();
 
-    testFileMetadataDTO =  objectMapper.readValue(testFileMetadataJSON, FileMetadataDTO.class);
+    testFileMetadataDTO = objectMapper.readValue(testFileMetadataJSON, FileMetadataDTO.class);
 
     testFileMetadataEntity = objectMapper.readValue(testFileMetadataJSON, FileMetadataEntity.class);
 
     newTestFileMetadataDTO = objectMapper.readValue(newTestFileMetadataJSON, FileMetadataDTO.class);
-
 
     when(fileMetadataRepository.save(any(FileMetadataEntity.class))).thenAnswer(invocation -> {
       return testFileMetadataEntity;
@@ -91,7 +91,7 @@ class FileMetadataServiceTest {
   }
 
   @Test
-  void storeKo() {
+  void storeKoFileAlreadyPresent() {
     Exception exception = assertThrows(FilenameAlreadyPresent.class, () -> {
       FileMetadataDTO stored = service.storeFileMetadata(testFileMetadataDTO);
     });
@@ -160,16 +160,38 @@ class FileMetadataServiceTest {
 
   @ParameterizedTest
   @ValueSource(strings = {
-      "{\"name\":\"test0\",\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTrx\":\"-1\",\"numAggregates\":2,\"amountAde\":900,\"amountRtd\":700,\"numChunks\":5,\"status\":0}",
-      "{\"name\":\"test0\",\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTrx\":\"1\",\"numAggregates\":-1,\"amountAde\":900,\"amountRtd\":700,\"numChunks\":5,\"status\":0}",
-      "{\"name\":\"test0\",\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTrx\":\"1\",\"numAggregates\":1,\"amountAde\":-1,\"amountRtd\":700,\"numChunks\":5,\"status\":0}",
-      "{\"name\":\"test0\",\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTrx\":\"1\",\"numAggregates\":1,\"amountAde\":900,\"amountRtd\":-1,\"numChunks\":5,\"status\":0}",
-      "{\"name\":\"test0\",\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTrx\":\"1\",\"numAggregates\":1,\"amountAde\":900,\"amountRtd\":700,\"numChunks\":-1,\"status\":0}"
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"wrong value\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"wrong value\",\"status\":0}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":\"wrong value\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"sender\":\"1234\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"sender\":\"123456\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5a\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"chunksTotal\":\"wrong value\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"chunksTotal\":-1}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"chunksLeft\":\"wrong value\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"chunksLeft\":-1}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"numTransactions\":\"wrong value\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"numTransactions\":-1}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"amountTransactions\":\"wrong value\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"amountTransactions\":-1}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"numAggregates\":\"wrong value\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"numAggregates\":-1}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"amountAggregates\":\"wrong value\"}",
+      "{\"name\":\"test0\",\"receiveTimestamp\":\"2020-08-06T11:19:16.500\",\"lastTransitionTimestamp\":\"2020-08-06T11:19:16.500\",\"status\":0,\"amountAggregates\":-1}",
   })
   void updateKoNonValidDTO(String body) throws JsonProcessingException {
     ObjectMapper objectMapper = new ObjectMapper();
 
-    FileMetadataDTO mapped = objectMapper.readValue(body, FileMetadataDTO.class);
+    //Assert true if Jackson cannot deserialize the JSON string
+    FileMetadataDTO mapped;
+    try {
+      mapped = objectMapper.readValue(body, FileMetadataDTO.class);
+    } catch (JsonMappingException e) {
+      Assertions.assertTrue(true);
+      return;
+    }
 
     assertThrows(DTOViolationException.class, () -> {
       service.updateFileMetadata(mapped);
@@ -178,8 +200,8 @@ class FileMetadataServiceTest {
 
   @ParameterizedTest
   @ValueSource(strings = {
-      "{\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTrx\":\"1\",\"numAggregates\":2,\"amountAde\":900,\"amountRtd\":700,\"numChunks\":5,\"status\":0}",
-      "{\"name\":\"\",\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTrx\":\"1\",\"numAggregates\":2,\"amountAde\":900,\"amountRtd\":700,\"numChunks\":5,\"status\":0}",
+      "{\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTransactions\":\"1\",\"numAggregates\":2,\"amountAggregates\":900,\"amountTransactions\":700,\"chunksTotal\":5,\"status\":0}",
+      "{\"name\":\"\",\"hash\":\"0c8795b2d35316c58136ec2c62056e23e9e620e3b6ec6653661db7a76abd38b5\",\"numTransactions\":\"1\",\"numAggregates\":2,\"amountAggregates\":900,\"amountTransactions\":700,\"chunksTotal\":5,\"status\":0}",
   })
   void updateKoEmptyFilename(String body) throws JsonProcessingException {
     ObjectMapper objectMapper = new ObjectMapper();
