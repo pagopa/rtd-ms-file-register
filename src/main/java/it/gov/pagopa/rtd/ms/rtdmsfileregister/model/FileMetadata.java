f5ccdac94f07f100d9713d62189a2ca1ba9f7053
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-07-14 15:30:58 +0200

    [RTD-188] update file metadata model

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/model/FileMetadata.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/model/FileMetadata.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -20,8 +17,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Getter
 @Setter
 public class FileMetadata {
-
-  //Common File Metadata
 
   @NotNull
   @NotBlank
@@ -40,46 +35,22 @@ public class FileMetadata {
   @NotNull
   private Integer status;
 
-  //CSTAR File Metadata
+  @NotNull
+  private Integer type;
 
-  @Pattern(regexp = "[a-zA-Z0-9]{5}",
+  @NotNull
+  private Integer application;
+
+  @Pattern(regexp = "([a-zA-Z0-9]{5})|ADE",
       message = "Sender code must be 5 alphanumeric char")
   private String sender;
 
-  //ADEACK File Metadata
-
-  private List<String> originalFilesList;
-
-  //RTD File Metadata
+  @NotNull
+  private Long size;
 
   @Pattern(regexp = "[a-zA-Z0-9]{64}",
       message = "Hash length must be 64 alphanumeric char")
   private String hash;
 
-  private String application;
-
-  @Min(value = 0, message = "Number of chunks total value must be positive")
-  private Integer chunksTotal;
-
-  @Min(value = 0, message = "Number of chunks left value must be positive")
-  private Integer chunksLeft;
-
-  //AppRTD File Metadata
-
-  @Min(value = 0, message = "numTransactions value must be positive")
-  private Integer numTransactions;
-
-  @Min(value = 0, message = "amountTransactions value must be positive")
-  private BigDecimal amountTransactions;
-
-  //AppADE File Metadata
-
-  @Min(value = 0, message = "numAggregates value must be positive")
-  private Integer numAggregates;
-
-  @Min(value = 0, message = "amountAggregates value must be positive")
-  private BigDecimal amountAggregates;
-
-  private List<String> senderAdeAckFilesList;
-
+  private String parent;
 }
