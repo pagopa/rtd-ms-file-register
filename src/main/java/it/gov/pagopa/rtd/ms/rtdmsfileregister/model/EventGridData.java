d0ed08f10c42240bd445bc1b15e484eddf7d1f35
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-07-20 16:01:07 +0200

    [TAE-120] remove required fields annotation

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/model/EventGridData.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/model/EventGridData.java
@@ -13,7 +13,7 @@ public class EventGridData {
   /**
    * The operation that triggered the event.
    */
-  @JsonProperty(value = "api", required = true)
+  @JsonProperty(value = "api")
   private String api;
 
   /**
@@ -25,19 +25,19 @@ public class EventGridData {
   /**
    * Service-generated request ID for the storage API operation.
    */
-  @JsonProperty(value = "requestId", required = true)
+  @JsonProperty(value = "requestId")
   private String requestId;
 
   /**
    * The value that you can use to run operations conditionally.
    */
-  @JsonProperty(value = "eTag", required = true)
+  @JsonProperty(value = "eTag")
   private String eTag;
 
   /**
    * The content type specified for the blob.
    */
-  @JsonProperty(value = "contentType", required = true)
+  @JsonProperty(value = "contentType")
   private String contentType;
 
   /**
@@ -49,56 +49,56 @@ public class EventGridData {
   /**
    * The type of blob. Valid values are either "BlockBlob" or "PageBlob".
    */
-  @JsonProperty(value = "blobType", required = true)
+  @JsonProperty(value = "blobType")
   private String blobType;
 
   /**
    * 	The offset in bytes of a write operation taken at the point where the event-triggering application completed writing to the file.
    */
-  @JsonProperty(value = "contentOffset", required = true)
+  @JsonProperty(value = "contentOffset")
   private String contentOffset;
 
   /**
    * The url of the file that will exist after the operation completes.
    */
-  @JsonProperty(value = "destinationUrl", required = true)
+  @JsonProperty(value = "destinationUrl")
   private String destinationUrl	;
 
   /**
    * The url of the file that exists before the operation is done.
    */
-  @JsonProperty(value = "sourceUrl", required = true)
+  @JsonProperty(value = "sourceUrl")
   private String sourceUrl;
 
   /**
    * The path to the blob.
    * If the client uses a Blob REST API, then the url has this structure: <storage-account-name>.blob.core.windows.net\<container-name>\<file-name>..
    */
-  @JsonProperty(value = "url", required = true)
+  @JsonProperty(value = "url")
   private String url;
 
   /**
    * True to run the operation on all child directories; otherwise False.
    */
-  @JsonProperty(value = "recursive", required = true)
+  @JsonProperty(value = "recursive")
   private String recursive;
 
   /**
    * An opaque string value representing the logical sequence of events for any particular blob name.
    */
-  @JsonProperty(value = "sequencer", required = true)
+  @JsonProperty(value = "sequencer")
   private String sequencer;
 
   /**
    * A string value representing the identity associated with the event.
    */
-  @JsonProperty(value = "identity", required = true)
+  @JsonProperty(value = "identity")
   private String identity	;
 
   /**
    * Diagnostic data occasionally included by the Azure Storage service..
    */
-  @JsonProperty(value = "storageDiagnostics", required = true)
+  @JsonProperty(value = "storageDiagnostics")
   private Object storageDiagnostics;
 
 
