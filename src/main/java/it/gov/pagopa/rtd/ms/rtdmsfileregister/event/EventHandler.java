3c2983a72826238c5b9294265439197717d8c3fb
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-08-08 13:45:31 +0200

    [TAE-143] add filter for created blob

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/event/EventHandler.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/event/EventHandler.java
@@ -27,7 +27,7 @@ public class EventHandler {
   @Bean
   public Consumer<Message<List<EventGridEvent>>> blobStorageConsumer(BlobRegisterAdapter blobRegisterAdapter) {
     return message -> message.getPayload().stream()
-//        .filter(e -> "Microsoft.Storage.BlobCreated".equals(e.getEventType()))
+        .filter(e -> "Microsoft.Storage.BlobCreated".equals(e.getEventType()))
         .map(blobRegisterAdapter::evaluateEvent)
         .collect(Collectors.toList());
   }
