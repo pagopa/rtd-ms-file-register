75253df83336547643dc0b1cf7871834a1a3ed22
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-08-11 16:20:59 +0200

    [TAE-148] add newline at the end of files

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/repository/FileMetadataRepository.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/repository/FileMetadataRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 /**
  * Data Access Object to manage all CRUD operations to the database This kind of repository
  * leverages object relational mapping (ORM), so it can be used only with relational DB.
- * 
+ *
  */
 public interface FileMetadataRepository extends MongoRepository<FileMetadataEntity, String> {
 
