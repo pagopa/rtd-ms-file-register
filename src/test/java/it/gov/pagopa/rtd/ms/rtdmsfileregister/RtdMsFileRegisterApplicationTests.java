43da979c5a49f3a59923b86296142e45049fb2c8
Andrea Morabito <andrea.morabito@pagopa.it>
2023-04-12 14:14:12 +0200

    [RTD-1584] improve context test

--- src/test/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/RtdMsFileRegisterApplicationTests.java
+++ src/test/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/RtdMsFileRegisterApplicationTests.java
@@ -1,16 +1,19 @@
 package it.gov.pagopa.rtd.ms.rtdmsfileregister;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestChannelBinderConfiguration.class)
 class RtdMsFileRegisterApplicationTests {
 
 	@Test
-	void contextLoads() {
-		assertTrue(true);
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
-
 }
