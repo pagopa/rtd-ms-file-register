bfea0ad5af397a2b39412164d74f1ddf1f06663a
Luca Consalvi <luca.consalvi@pagopa.it>
2024-01-24 15:54:47 +0100

    [RTD-2149] remove runtime hints

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/AppConfiguration.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/AppConfiguration.java
@@ -1,21 +1,14 @@
 package it.gov.pagopa.rtd.ms.rtdmsfileregister;
 
-import it.gov.pagopa.rtd.ms.rtdmsfileregister.AppConfiguration.CustomRuntimeHints;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.adapter.FileChangedEventListener;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.domain.DefaultNamingPolicy;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.domain.NamingConventionPolicy;
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.domain.events.FileChangedFactory;
-
-import org.springframework.aot.hint.MemberCategory;
-import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.aot.hint.TypeReference;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Configuration
-@ImportRuntimeHints(CustomRuntimeHints.class)
 public class AppConfiguration {
 
   private static final String PROJECTOR_BINDING_NAME = "fileRegisterProjector-out-0";
@@ -35,19 +28,4 @@ public class AppConfiguration {
     return new FileChangedEventListener(streamBridge, PROJECTOR_BINDING_NAME);
   }
 
-
-  // Probably due to an instability of snapshot version some reflection data are
-  // missing,
-  // this register the missing class.
-  public static class CustomRuntimeHints implements RuntimeHintsRegistrar {
-
-    @Override
-    public void registerHints(org.springframework.aot.hint.RuntimeHints hints,
-        ClassLoader classLoader) {
-      hints.reflection().registerType(
-          TypeReference
-              .of("org.springframework.integration.config.ConverterRegistrar$IntegrationConverterRegistration"),
-          MemberCategory.values());
-    }
-  }
 }
--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/event/EventHandler.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/event/EventHandler.java
@@ -6,9 +6,6 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import lombok.Getter;
-
-import org.springframework.aot.hint.annotation.Reflective;
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
@@ -26,7 +23,6 @@ public class EventHandler {
    * @return a consumer for Event Grid events.
    */
   @Bean
-  @RegisterReflectionForBinding
   public Consumer<Message<List<EventGridEvent>>> blobStorageConsumer(BlobRegisterAdapter blobRegisterAdapter) {
     return message -> message.getPayload().stream()
         .filter(e -> "Microsoft.Storage.BlobCreated".equals(e.getEventType()))
