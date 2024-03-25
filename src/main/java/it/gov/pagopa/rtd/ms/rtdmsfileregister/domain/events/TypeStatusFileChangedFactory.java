464506b8f9c318f7d207516376a5e1954014ffd4
Andrea Morabito <andrea.morabito@pagopa.it>
2024-03-25 16:09:30 +0100

    propagate the container information to projection queue

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/domain/events/TypeStatusFileChangedFactory.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmsfileregister/domain/events/TypeStatusFileChangedFactory.java
@@ -1,14 +1,12 @@
 package it.gov.pagopa.rtd.ms.rtdmsfileregister.domain.events;
 
 import it.gov.pagopa.rtd.ms.rtdmsfileregister.model.FileMetadata;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 /**
- * This is a factory which produce file changed events based on
- * file metadata's type and status
+ * This is a factory which produce file changed events based on file metadata's type and status
  */
 class TypeStatusFileChangedFactory implements FileChangedFactory {
 
@@ -26,7 +24,8 @@ class TypeStatusFileChangedFactory implements FileChangedFactory {
   @Override
   public Optional<FileChanged> createFrom(FileMetadata file) {
     return Optional.ofNullable(statusConversionMap.get(lookupKey(file)))
-            .map(type -> new FileChanged(file.getParent(), file.getSender(), file.getSize(), file.getReceiveTimestamp(), type));
+        .map(type -> new FileChanged("/" + file.getContainer() + "/" + file.getParent(),
+            file.getSender(), file.getSize(), file.getReceiveTimestamp(), type));
   }
 
   private String lookupKey(FileMetadata fileMetadata) {
